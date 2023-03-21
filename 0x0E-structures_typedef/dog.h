88e9a2d99058d034bc13fcb240ce973b61852b49
Spookydee <dfafowora@gmail.com>
2023-03-21 10:52:13 -0700

    uod

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - function to typedef struct dog
+ * struct dog - Struct dog
  * @name: character pointer
  * @age: float
  * @owner: character pointer
+ * Description: this struct is for dog
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif
+#endif /*DOG_H*/
