a7d5cf95f0802879fbfb9cf6a98d722c41893392
spookydee <dfafowora@gmail.com>
2023-06-08 18:56:27 -0700

    update

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,6 +2,7 @@
 #ifndef MAIN_H
 
 void _puts_recursion(char *s);
+int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
@@ -10,6 +11,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-int _putchar(char c);
 
 #endif
